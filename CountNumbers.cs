7e4d7bad4b29e7aee55d3fb003138d1783b41b35
SfarxX <alupachiov@mail.ru>
2024-08-11 02:03:14 +0300

    Implemented MenuForm

---
+++ CountNumbers.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphic
+{
+    internal class CountNumbers
+    {
+        enum Digits
+        {
+            k1 = 0,
+            k2 = 1,
+            k3 = 2,
+            k4 = 3,
+            k5 = 4,
+            k6 = 5,
+            k7 = 6,
+            k8 = 7,
+            k9 = 8
+        }
+        public static int[] digitsArray = new int [9];
+        static public void FillCounters(int N)
+        {
+
+            for (int i = 0; i <= N; i++)
+            {
+                double Power = Math.Pow(2, i);
+                CountDigit(GetFirstDigit(Power));
+            }
+        }
+        static public int GetFirstDigit(double number)
+        {
+            while (number >= 10) number /= 10;
+            return (int)number;
+        }
+        static public void CountDigit(int digit)
+        {
+            switch (digit)
+            {
+                case 1:
+                    digitsArray[(int)Digits.k1]++;
+                    break;
+                case 2:
+                    digitsArray[(int)Digits.k2]++;
+                    break;
+                case 3:
+                    digitsArray[(int)Digits.k3]++;
+                    break;
+                case 4:
+                    digitsArray[(int)Digits.k4]++;
+                    break;
+                case 5:
+                    digitsArray[(int)Digits.k5]++;
+                    break;
+                case 6:
+                    digitsArray[(int)Digits.k6]++;
+                    break;
+                case 7:
+                    digitsArray[(int)Digits.k7]++;
+                    break;
+                case 8:
+                    digitsArray[(int)Digits.k8]++;
+                    break;
+                case 9:
+                    digitsArray[(int)Digits.k9]++;
+                    break;
+                default:
+                    break;
+            }
+        }
+        static public int GetDigitCount(int digit)
+        {
+            return digitsArray[digit - 1];
+        }
+    }
+}
---
+++ MenuForm.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace Graphic
+{
+    partial class MenuForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint1 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(0D, "0,0");
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series8 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series9 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.менюToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.построитьГрафикToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.очиститьГрафикToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // chart
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            legend1.Title = "Первые цифры";
+            this.chart.Legends.Add(legend1);
+            this.chart.Location = new System.Drawing.Point(1, 12);
+            this.chart.Name = "chart";
+            series1.BorderColor = System.Drawing.Color.White;
+            series1.ChartArea = "ChartArea1";
+            series1.Color = System.Drawing.Color.DodgerBlue;
+            series1.CustomProperties = "DrawSideBySide=True";
+            series1.IsValueShownAsLabel = true;
+            series1.LabelBackColor = System.Drawing.Color.White;
+            series1.Legend = "Legend1";
+            series1.Name = "Единицы";
+            dataPoint1.IsValueShownAsLabel = true;
+            dataPoint1.Label = "";
+            dataPoint1.MarkerBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            series1.Points.Add(dataPoint1);
+            series1.SmartLabelStyle.IsMarkerOverlappingAllowed = true;
+            series1.SmartLabelStyle.MovingDirection = ((System.Windows.Forms.DataVisualization.Charting.LabelAlignmentStyles)(((((System.Windows.Forms.DataVisualization.Charting.LabelAlignmentStyles.Top | System.Windows.Forms.DataVisualization.Charting.LabelAlignmentStyles.Bottom) 
+            | System.Windows.Forms.DataVisualization.Charting.LabelAlignmentStyles.TopRight) 
+            | System.Windows.Forms.DataVisualization.Charting.LabelAlignmentStyles.BottomLeft) 
+            | System.Windows.Forms.DataVisualization.Charting.LabelAlignmentStyles.BottomRight)));
+            series1.YValuesPerPoint = 2;
+            series2.ChartArea = "ChartArea1";
+            series2.Color = System.Drawing.Color.Lime;
+            series2.IsValueShownAsLabel = true;
+            series2.Legend = "Legend1";
+            series2.Name = "Двойки";
+            series3.ChartArea = "ChartArea1";
+            series3.Color = System.Drawing.Color.Fuchsia;
+            series3.IsValueShownAsLabel = true;
+            series3.Legend = "Legend1";
+            series3.Name = "Тройки";
+            series4.BorderColor = System.Drawing.Color.Black;
+            series4.ChartArea = "ChartArea1";
+            series4.Color = System.Drawing.Color.Yellow;
+            series4.IsValueShownAsLabel = true;
+            series4.Legend = "Legend1";
+            series4.Name = "Четвёрки";
+            series5.ChartArea = "ChartArea1";
+            series5.IsValueShownAsLabel = true;
+            series5.Legend = "Legend1";
+            series5.Name = "Пятёрки";
+            series6.ChartArea = "ChartArea1";
+            series6.IsValueShownAsLabel = true;
+            series6.Legend = "Legend1";
+            series6.Name = "Шестёрки";
+            series7.ChartArea = "ChartArea1";
+            series7.IsValueShownAsLabel = true;
+            series7.Legend = "Legend1";
+            series7.Name = "Семёрки";
+            series8.ChartArea = "ChartArea1";
+            series8.IsValueShownAsLabel = true;
+            series8.Legend = "Legend1";
+            series8.Name = "Восьмёрки";
+            series9.ChartArea = "ChartArea1";
+            series9.IsValueShownAsLabel = true;
+            series9.Legend = "Legend1";
+            series9.Name = "Девятки";
+            this.chart.Series.Add(series1);
+            this.chart.Series.Add(series2);
+            this.chart.Series.Add(series3);
+            this.chart.Series.Add(series4);
+            this.chart.Series.Add(series5);
+            this.chart.Series.Add(series6);
+            this.chart.Series.Add(series7);
+            this.chart.Series.Add(series8);
+            this.chart.Series.Add(series9);
+            this.chart.Size = new System.Drawing.Size(903, 506);
+            this.chart.TabIndex = 0;
+            this.chart.Text = "chart";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Visible;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.менюToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(906, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // менюToolStripMenuItem
+            // 
+            this.менюToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.построитьГрафикToolStripMenuItem,
+            this.очиститьГрафикToolStripMenuItem});
+            this.менюToolStripMenuItem.Name = "менюToolStripMenuItem";
+            this.менюToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.менюToolStripMenuItem.Text = "Меню";
+            // 
+            // построитьГрафикToolStripMenuItem
+            // 
+            this.построитьГрафикToolStripMenuItem.Name = "построитьГрафикToolStripMenuItem";
+            this.построитьГрафикToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
+            this.построитьГрафикToolStripMenuItem.Text = "Построить график";
+            this.построитьГрафикToolStripMenuItem.Click += new System.EventHandler(this.построитьГрафикToolStripMenuItem_Click);
+            // 
+            // очиститьГрафикToolStripMenuItem
+            // 
+            this.очиститьГрафикToolStripMenuItem.Name = "очиститьГрафикToolStripMenuItem";
+            this.очиститьГрафикToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
+            this.очиститьГрафикToolStripMenuItem.Text = "Очистить график";
+            this.очиститьГрафикToolStripMenuItem.Click += new System.EventHandler(this.очиститьГрафикToolStripMenuItem_Click);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(400, 524);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(100, 20);
+            this.textBox.TabIndex = 2;
+            this.textBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(158, 521);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(236, 29);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "СТЕПЕНЬ ДВОЙКИ";
+            // 
+            // MenuForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(906, 548);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.chart);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MenuForm";
+            this.Text = "График";
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem менюToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem построитьГрафикToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem очиститьГрафикToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
