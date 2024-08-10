13e85e71cee086affc384ec8dbf44e04cc7999bf
anton.lisitsyn <anton.lisitsyn.a@gmail.com>
2024-08-10 10:30:15 +0300

    just initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Graphic
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Main app run point
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //TODO: - Add implementation here
+        }
+    }
+}
